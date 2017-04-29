63076cf1df5cf51a9513ab368c9f2e30b188a6c8
Tony <tonykrub@hotmail.com>
2017-04-29 01:25:23 +0000

    week1 day3 workshop

--- answers.js
+++ answers.js
@@ -9,10 +9,32 @@ function printPositives(arr){
 }
 //printPositives([1, -8, 999, 45, -35, 9]);
 
+// or could do like the below
+function printPositives(arr){
+    return arr.forEach(function positiveNum(ele) {
+        if (ele > 0) {
+            console.log(ele);
+        }
+    })
+}
+//console.log(printPositives([-2, -8, 6, 9, 99]))
+
 
 // 2) Similar to the previous exercise, write a function 
 // called getPositives that takes an array and uses the filter 
 // method to return a new array with only the positive numbers.
+function getPositives(arr) {
+    return arr.filter(function positiveNum(num) {
+        var array = [];
+        if (num >0) {
+            array.push(num);
+        }
+        return array;
+    })
+}
+//console.log(getPositives([5, 9, 99, -6, -5]));
+
+// or could do like the below. 
 function getPositives(arr) {
     return arr.filter(function filtered(num) {
         if (num > 0) {
@@ -26,6 +48,18 @@ function getPositives(arr) {
 // 2B) Re-do exercise 1 by first filtering the input array, and 
 // then printing the numbers from the filtered array. Your code
 // will look something like: `return arr.filter(…).forEach(…)
+
+function printPositives(arr){
+    return arr.filter(function filtered(num) {
+        if (num > 0) {
+            return num
+        }
+    }).forEach(function printNum(num) {
+        console.log(num);
+    })
+}
+//printPositives([9, 6, -99, -5, 19])
+// or
 function positiveNum(arr) {
     return arr.filter(function filteredNum(num) {
         if (num > 0) {
@@ -47,6 +81,7 @@ function positiveNum(arr) {
 // this question.
 // NOTE 2: This is a bit of a trick question, the answer is a 
 // one-liner :)
+
 function filterArray(arr, func) {
     return arr.filter(function truthyVal(element) {
         if (element) {
@@ -62,6 +97,23 @@ function filterArray(arr, func) {
 // should use Array.prototype.reduce to do your work.
 // Hint: You can use String.prototype.split to split the string 
 // into an array of words.
+function longestWord(str) {
+    var split = str.split(' ');
+    return split.reduce(function(a, b) {
+        if (a.length > b.length) {
+            return a
+        } 
+        else if (a.lenth < b.length) {
+            return b
+        }
+        else {
+            console.log("both " + a + " and " + b + "are the longest")
+        }
+    }, 0)
+    
+}
+//console.log(longestWord("I love you Anna"));
+// or
 function longestWord(str) {
     var spl = str.split(" ");
     return spl.reduce(function(a, b) {
@@ -86,6 +138,18 @@ function longestWord(str) {
 // to use it to split a string by character. Try to Google it :)
 // Hint 2: You can create an array of vowels and use 
 // Array.prototype.indexOf to check if the current letter is a vowel.
+function countVowels(str) {
+    var split = str.split('');
+    var arr = [];
+    return split.reduce(function(letter){
+        if (letter === "a" || letter === "e" || letter === "i"|| letter === "o" || letter === "u") {
+            arr.push(letter)
+        }
+        return arr.length;
+    }, 0)
+}
+//console.log(countVowels("I love you aeiou"));
+// or 
 function countVowels(str){
     var splitedStr = str.toLowerCase().split('');
     return splitedStr.reduce(function(accumulator, currentValue) { 
@@ -134,6 +198,22 @@ function countVowels(str){
 // Hint: Javascript has a special value called Infinity, which is
 // higher than any other number. See if you can initialize your 
 // reduce accumulator with Infinity and -Infinity :)
+function highLow(arr){
+    return arr.reduce(function(a, b) {
+        if (b > a.highest) {
+            a.highest === b;
+        }
+        if (b < a.lowest) {
+            a.lowest === b;
+        }
+    }, [  // dont know why i can use [] here??
+        highest = -Infinity,
+        lowest = +Infinity 
+    ])
+}
+//console.log(highLow([-2, -8, 6, 9, 99, 999, -69]))
+// the above is what i tried to do by myself to practice, and it works
+// or 
 function highLow(arr) {
     return arr.reduce(function(counterVal,curVal) {
         if (counterVal.highest < curVal) {
@@ -210,11 +290,179 @@ function highLowTwo(arr) {
 // When you print your object on the console, your keys may be 
 // displayed in a different order, and it does not matter.
 function countChars(str) {
-    var splitStr = str.toLowerCase().split("");
-    return splitStr.reduce(function repeatedltr(a,b) {
-        if (a = b) {
-            count = count + 1;
+    var splitStr = str.toLowerCase().replace(" ","").split(""); // also have to replace the space between words. that is why we use 'replace' here.
+    return splitStr.reduce(function repeatedLetter(a,b) { // 'a' is like 'counter' and 'b' is like each item in the array that we are checking.
+        if (!a[b]) {  // Here we check if that letter already exists, because we don't know yet if that letter exists already or not. This means if there is no 'b', then count 1 because it does not yet exist
+            a[b] = 1; // we use [] to make it 'dynamic' because if we use 'dot' like in previous exercises (a.b instead of a[b]), it will mean 'b' is the property of that object 'a'. But here 'a' does not have any property. So we cannot use dot here.
         }
-    },0)
+        else {
+            a[b] += 1; // this means if 'b' already exists, then count 1 more.
+        }
+        return a;
+    },{})
+}
+//console.log(countChars("HeLlo World"));
+
+
+/* 9) Starting on week 3 of the bootcamp, we will be learning 
+about databases. Databases are useful for saving data for 
+long periods of time.
+Contrary to a JavaScript program, where all the data 
+disappears when the program terminates, databases keep their 
+data "forever".
+In this exercise, we’re going to imagine that we are storing 
+people information in a database, and that we queried our 
+database to retrieve a list of people.
+Our database returns to us an array of people objects, and 
+each of them has a unique ID that the database uses to refer 
+to it.
+Here’s what our person data could look like:
+[
+  {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+]
+For this exercise, we want to use Array.prototype.reduce to 
+transform our array of people into an object, keyed with the 
+unique ID.The end result should look like this:
+{
+  "KeXoYg92is": {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  "NkALmSWtUp": {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  "m7LPbJYSUg": {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+}
+This object could be useful if we are often looking up people 
+by their unique ID.
+Write a function called peopleById that takes an array of 
+people and returns an object where each person is keyed by 
+their unique ID.
+You have effectively created what we call an index, not unlike 
+the one you have in your phonebook.*/
+function peopleById (arr) {
+    return arr.reduce(function sortedByID(a,b) {
+        a[b.id] = b;  // it is like we search for 'b' by ID and put that equals to the whole 'b' in order to display all properties of the object.
+        return a;
+    },{})
 }
 
+var arr = [
+  {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+]
+//console.log(peopleById (arr));
+
+
+/* 10) Expanding on the previous exercise, this time we are 
+going to create an index on first names. Notice how in the 
+previous exercise, each ID was unique. In this case, two 
+people have the same first name.
+We want to create a function called peopleByFirstName that 
+will take an array of people and return something that looks 
+like this:
+{
+  "John": [
+    {
+      "id": "KeXoYg92is",
+      "firstName": "John",
+      "lastName": "Smith",
+      "email": "john@smith.com"
+    },
+    {
+      "id": "m7LPbJYSUg",
+      "firstName": "John",
+      "lastName": "Vader",
+      "email": "vader@darkside.com"
+    }
+  ],
+  "Donald": [
+    {
+      "id": "NkALmSWtUp",
+      "firstName": "Donald",
+      "lastName": "Duck",
+      "email": "don@disney.com"
+    }
+  ]
+}
+You have effectively created an index on first name. This lets 
+you find all people called, say, “John” without having to look
+through the whole results. */
+
+function peopleByFirstName(arr) {
+    return arr.reduce(function sortedPeople(a,b) {
+        if (!a[b.firstName]) {
+            a[b.firstName] = []; // because we want the result to be 'firstName' equals an array. That is why we set a[b.firstName] equal an array. And we cannot use dot (a.b.firstName) here. And it has to be a[b.firstName], not just b.firstName because at the end we return a to get the value of a.
+            a[b.firstName].push(b);
+        }
+        else {
+            a[b.firstName].push(b);
+        }
+        return a;
+    },{})
+}
+
+var arr = [
+  {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+]
+//console.log(peopleByFirstName(arr));
